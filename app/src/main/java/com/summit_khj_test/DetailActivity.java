1572ba90e51d6c8d193b2b17ed9b636e00842274
Kim Hak Jin <fjtl36@naver.com>
2020-10-30 17:58:05 +0900

    intent에 데이터를 담아 전송

--- app/src/main/java/com/summit_khj_test/DetailActivity.java
+++ app/src/main/java/com/summit_khj_test/DetailActivity.java
@@ -1,6 +1,8 @@
 package com.summit_khj_test;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,24 @@ public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
+    private String mForecast;
+    private TextView mWeatherDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_display_weather);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        //만약 intent가 존재하고 Intent.EXTRA_TEXT가 존재할 경우
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+        }
     }
 }
