3e9d8caaaed0080c7fdfd61993481f7493df1003
Kim Hak Jin <fjtl36@naver.com>
2020-10-31 20:07:16 +0900

    AsyncTaskLoader를 CursorLoader로 교체

--- app/src/main/java/com/summit_khj_test/ForecastAdapter.java
+++ app/src/main/java/com/summit_khj_test/ForecastAdapter.java
@@ -1,6 +1,8 @@
 package com.summit_khj_test;
 
+import android.content.ContentProvider;
 import android.content.Context;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,11 +12,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.summit_khj_test.utilities.SunshineDateUtils;
+import com.summit_khj_test.utilities.SunshineWeatherUtils;
+
 
 //리사이클러뷰 어댑터
 public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder>{
 
-    private String[] mWeatherData;
+    private final Context mContext;
 
     private final ForecastAdapterOnClickHandler mClickHandler;
 
@@ -23,56 +28,76 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
         void onClick(String weatherForDay);
     }
 
+    private Cursor mCursor;
+
     //생성자
-    public ForecastAdapter(ForecastAdapterOnClickHandler clickHandler) {
+    public ForecastAdapter(Context context, ForecastAdapterOnClickHandler clickHandler) {
+        mContext = context;
         mClickHandler = clickHandler;
     }
 
-    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements OnClickListener {
-        public final TextView mWeatherTextView;
+    class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements OnClickListener {
+        final TextView weatherSummary;
 
-        public ForecastAdapterViewHolder(View view) {
+        ForecastAdapterViewHolder(View view) {
             super(view);
-            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+            weatherSummary = (TextView) view.findViewById(R.id.tv_weather_data);
             view.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-            int adapterPosition = getAdapterPosition();
-            String weatherForDay = mWeatherData[adapterPosition];
+            String weatherForDay = weatherSummary.getText().toString();
             mClickHandler.onClick(weatherForDay);
         }
     }
 
     @Override
     public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        Context context = viewGroup.getContext();
-        int layoutIdForListItem = R.layout.forcast_list_item;
-        LayoutInflater inflater = LayoutInflater.from(context);
-        boolean shouldAttachToParentImmediately = false;
+        View view = LayoutInflater
+                .from(mContext)
+                .inflate(R.layout.forcast_list_item, viewGroup, false);
 
-        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        view.setFocusable(true);
+
         return new ForecastAdapterViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ForecastAdapterViewHolder forecastAdapterViewHolder, int position) {
-        String weatherForThisDay = mWeatherData[position];
-        forecastAdapterViewHolder.mWeatherTextView.setText(weatherForThisDay);
+        mCursor.moveToPosition(position);
+
+        //커서로부터 날짜 데이터를 읽음
+        long dateInMillis = mCursor.getLong(MainActivity.INDEX_WEATHER_DATE);
+
+        String dateString = SunshineDateUtils.getFriendlyDateString(mContext, dateInMillis, false);
+
+        int weatherId = mCursor.getInt(MainActivity.INDEX_WEATHER_CONDITION_ID);
+        String description = SunshineWeatherUtils.getStringForWeatherCondition(mContext, weatherId);
+
+        //최고, 최저 기온
+        double highInCelsius = mCursor.getDouble(MainActivity.INDEX_WEATHER_MAX_TEMP);
+        double lowInCelsius = mCursor.getDouble(MainActivity.INDEX_WEATHER_MIN_TEMP);
+
+        String highAndLowTemperature =
+                SunshineWeatherUtils.formatHighLows(mContext, highInCelsius, lowInCelsius);
+
+        String weatherSummary = dateString + " - " + description + " - " + highAndLowTemperature;
+
+        forecastAdapterViewHolder.weatherSummary.setText(weatherSummary);
     }
 
     //아이템들의 갯수를 반환
     @Override
     public int getItemCount() {
-        if (mWeatherData == null) {
+        if (null == mCursor)
             return 0;
-        }
-        return mWeatherData.length;
+        return mCursor.getCount();
     }
 
-    public void setWeatherData(String[] weatherData) {
-        mWeatherData = weatherData;
+    //새로운 데이터가 로드될 경우 호툴
+    void swapCursor(Cursor newCursor) {
+        mCursor = newCursor;
         notifyDataSetChanged();
     }
 }
