dcac548c5ee268b4183de8fcec85e5d45cf92087
Kim Hak Jin <fjtl36@naver.com>
2020-10-31 12:32:35 +0900

    SettingFragment.java 생성 및 PreferenceFragmentCompat 상속

---
+++ app/src/main/java/com/summit_khj_test/SettingsFragment.java
@@ -0,0 +1,13 @@
+package com.summit_khj_test;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.pref_general);
+    }
+}
