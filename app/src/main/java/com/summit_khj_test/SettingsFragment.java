c34218cd7c0b33d10534ea3051456d02fd4c2a76
Kim Hak Jin <fjtl36@naver.com>
2020-10-31 14:28:55 +0900

    SettingFragment.java 구현 (요약정보 변경 및 생명주기에 따른 changeListener 등록 및 해지)

--- app/src/main/java/com/summit_khj_test/SettingsFragment.java
+++ app/src/main/java/com/summit_khj_test/SettingsFragment.java
@@ -1,13 +1,75 @@
 package com.summit_khj_test;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.pref_general);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        int count = preferenceScreen.getPreferenceCount();
+
+        for (int i = 0; i < count; i++) {
+            Preference p = preferenceScreen.getPreference(i);
+            if (!(p instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+    }
+
+    //환경설정 아래오는 요약정보 표시
+    private void setPreferenceSummary(Preference preference, Object value) {
+        String stringValue = value.toString();
+        String key = preference.getKey();
+
+        if (preference instanceof ListPreference) {
+            //목록 기본 설정의 경우 항목목록에서 올바른 표시 값을 찾음
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            //다른 환경 설정의 경우 단순 문자열을 세팅
+            preference.setSummary(stringValue);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if(null != preference) {
+            if(!(preference instanceof CheckBoxPreference)) {
+                setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //변경리스너 등록
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        //변경리스너 등록 해제
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
