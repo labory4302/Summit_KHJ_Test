2339ed7ffc23753509c26a4277ab9714a2536ab2
Kim Hak Jin <fjtl36@naver.com>
2020-11-01 11:20:36 +0900

    SunshineSyncTask.java 에서 필요한 메소드를 OpenWeatherJsonUtils.java 에 생성

--- app/src/main/java/com/summit_khj_test/utilities/OpenWeatherJsonUtils.java
+++ app/src/main/java/com/summit_khj_test/utilities/OpenWeatherJsonUtils.java
@@ -1,6 +1,9 @@
 package com.summit_khj_test.utilities;
 
+import android.content.ContentValues;
 import android.content.Context;
+
+import com.summit_khj_test.data.SunshinePreferences;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -9,6 +12,31 @@ import org.json.JSONObject;
 import java.net.HttpURLConnection;
 
 public class OpenWeatherJsonUtils {
+    private static final String OWM_CITY = "city";
+    private static final String OWM_COORD = "coord";
+
+    /* 좌표 값 */
+    private static final String OWM_LATITUDE = "lat";
+    private static final String OWM_LONGITUDE = "lon";
+
+    /* 날씨 정보들 */
+    private static final String OWM_LIST = "list";
+
+    private static final String OWM_PRESSURE = "pressure";
+    private static final String OWM_HUMIDITY = "humidity";
+    private static final String OWM_WINDSPEED = "speed";
+    private static final String OWM_WIND_DIRECTION = "deg";
+
+    private static final String OWM_TEMPERATURE = "temp";
+
+    private static final String OWM_MAX = "max";
+    private static final String OWM_MIN = "min";
+
+    private static final String OWM_WEATHER = "weather";
+    private static final String OWM_WEATHER_ID = "id";
+
+    private static final String OWM_MESSAGE_CODE = "cod";
+
     public static String[] getSimpleWeatherStringsFromJson(Context context, String forecastJsonStr)
             throws JSONException {
 
@@ -27,6 +55,7 @@ public class OpenWeatherJsonUtils {
         final String OWM_DESCRIPTION = "main";
 
         final String OWM_MESSAGE_CODE = "cod";
+
 
         //날씨들을 담을 배열
         String[] parsedWeatherData = null;
@@ -88,5 +117,106 @@ public class OpenWeatherJsonUtils {
         }
 
         return parsedWeatherData;
+    }
+
+    public static ContentValues[] getWeatherContentValuesFromJson(Context context, String forecastJsonStr)
+            throws JSONException {
+
+        JSONObject forecastJson = new JSONObject(forecastJsonStr);
+
+        //통신에러 유무 확인
+        if (forecastJson.has(OWM_MESSAGE_CODE)) {
+            int errorCode = forecastJson.getInt(OWM_MESSAGE_CODE);
+
+            switch (errorCode) {
+                case HttpURLConnection.HTTP_OK:
+                    break;
+                case HttpURLConnection.HTTP_NOT_FOUND:
+                    /* Location invalid */
+                    return null;
+                default:
+                    /* Server probably down */
+                    return null;
+            }
+        }
+
+        JSONArray jsonWeatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+        JSONObject cityJson = forecastJson.getJSONObject(OWM_CITY);
+
+        JSONObject cityCoord = cityJson.getJSONObject(OWM_COORD);
+        double cityLatitude = cityCoord.getDouble(OWM_LATITUDE);
+        double cityLongitude = cityCoord.getDouble(OWM_LONGITUDE);
+
+        //프리퍼런스에 위도 경도 설정
+        SunshinePreferences.setLocationDetails(context, cityLatitude, cityLongitude);
+
+        ContentValues[] weatherContentValues = new ContentValues[jsonWeatherArray.length()];
+
+        //요청된 위치의 현지 시간을 기준으로 일일 예측을 반환
+        long normalizedUtcStartDay = SunshineDateUtils.getNormalizedUtcDateForToday();
+
+        for (int i = 0; i < jsonWeatherArray.length(); i++) {
+
+            long dateTimeMillis;
+            double pressure;
+            int humidity;
+            double windSpeed;
+            double windDirection;
+
+            double high;
+            double low;
+
+            int weatherId;
+
+            /* Get the JSON object representing the day */
+            JSONObject dayForecast = jsonWeatherArray.getJSONObject(i);
+
+            /*
+             * We ignore all the datetime values embedded in the JSON and assume that
+             * the values are returned in-order by day (which is not guaranteed to be correct).
+             */
+            dateTimeMillis = normalizedUtcStartDay + SunshineDateUtils.DAY_IN_MILLIS * i;
+
+            pressure = dayForecast.getDouble(OWM_PRESSURE);
+            humidity = dayForecast.getInt(OWM_HUMIDITY);
+            windSpeed = dayForecast.getDouble(OWM_WINDSPEED);
+            windDirection = dayForecast.getDouble(OWM_WIND_DIRECTION);
+
+            /*
+             * Description is in a child array called "weather", which is 1 element long.
+             * That element also contains a weather code.
+             */
+            JSONObject weatherObject =
+                    dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+
+            weatherId = weatherObject.getInt(OWM_WEATHER_ID);
+
+            /*
+             * Temperatures are sent by Open Weather Map in a child object called "temp".
+             *
+             * Editor's Note: Try not to name variables "temp" when working with temperature.
+             * It confuses everybody. Temp could easily mean any number of things, including
+             * temperature, temporary variable, temporary folder, temporary employee, or many
+             * others, and is just a bad variable name.
+             */
+            JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+            high = temperatureObject.getDouble(OWM_MAX);
+            low = temperatureObject.getDouble(OWM_MIN);
+
+            ContentValues weatherValues = new ContentValues();
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_DATE, dateTimeMillis);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_HUMIDITY, humidity);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_PRESSURE, pressure);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_WIND_SPEED, windSpeed);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_DEGREES, windDirection);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_MAX_TEMP, high);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_MIN_TEMP, low);
+            weatherValues.put(WeatherContract.WeatherEntry.COLUMN_WEATHER_ID, weatherId);
+
+            weatherContentValues[i] = weatherValues;
+        }
+
+        return weatherContentValues;
     }
 }
