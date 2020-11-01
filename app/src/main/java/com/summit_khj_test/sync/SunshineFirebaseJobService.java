9e0a75180c97608fff2b25811fd6e2a41a942a56
Kim Hak Jin <fjtl36@naver.com>
2020-11-01 13:19:45 +0900

    오타 수정 (중복으로 class생성)

--- app/src/main/java/com/summit_khj_test/sync/SunshineFirebaseJobService.java
+++ app/src/main/java/com/summit_khj_test/sync/SunshineFirebaseJobService.java
@@ -8,45 +8,43 @@ import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
 import com.firebase.jobdispatcher.RetryStrategy;
 
-public class SunshineFirebaseJobService {
-    public class SunshineFirebaseJobService extends JobService {
+public class SunshineFirebaseJobService extends JobService {
 
-        private AsyncTask<Void, Void, Void> mFetchWeatherTask;
+    private AsyncTask<Void, Void, Void> mFetchWeatherTask;
 
-        //운영체제가 작업을 실행할 준비가 된다면 자동으로 실행
-        @Override
-        public boolean onStartJob(final JobParameters jobParameters) {
-            mFetchWeatherTask = new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(Void... voids) {
-                    Context context = getApplicationContext();
-                    SunshineSyncTask.syncWeather(context);
-                    return null;
-                }
-
-                @Override
-                protected void onPostExecute(Void aVoid) {
-                    //작업이 끝났음을 의미
-                    //true는 지금은 이 작업을 수행할 수 없으니 향후에 다시 시작시켜달라는 것을 의미
-                    jobFinished(jobParameters, false);
-                }
-            }.execute();
-
-            //true : 작업이 아직 진행중
-            //flase : 작압이 아직 완료되지 않음
-            return true;
-        }
-
-        //작업이 중단될 필요가 있을 경우 호출
-        @Override
-        public boolean onStopJob(JobParameters jobParameters) {
-            if (mFetchWeatherTask != null) {
-                mFetchWeatherTask.cancel(true);
+    //운영체제가 작업을 실행할 준비가 된다면 자동으로 실행
+    @Override
+    public boolean onStartJob(final JobParameters jobParameters) {
+        mFetchWeatherTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Context context = getApplicationContext();
+                SunshineSyncTask.syncWeather(context);
+                return null;
             }
 
-            //true : 향후에 다시 실행되어야 함을 의미
-            //false : 향후에 다시 시작할 필요가 없음
-            return true;
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                //작업이 끝났음을 의미
+                //true는 지금은 이 작업을 수행할 수 없으니 향후에 다시 시작시켜달라는 것을 의미
+                jobFinished(jobParameters, false);
+            }
+        }.execute();
+
+        //true : 작업이 아직 진행중
+        //flase : 작압이 아직 완료되지 않음
+        return true;
+    }
+
+    //작업이 중단될 필요가 있을 경우 호출
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        if (mFetchWeatherTask != null) {
+            mFetchWeatherTask.cancel(true);
         }
+
+        //true : 향후에 다시 실행되어야 함을 의미
+        //false : 향후에 다시 시작할 필요가 없음
+        return true;
     }
 }
