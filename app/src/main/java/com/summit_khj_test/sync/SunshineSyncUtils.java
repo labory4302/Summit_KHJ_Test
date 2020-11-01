cee354ffae00dc722ad6b68f90fc866b2da9eb9e
Kim Hak Jin <fjtl36@naver.com>
2020-11-01 12:56:43 +0900

    데이터베이스에 데이터가 없을 시 서비스 실행

--- app/src/main/java/com/summit_khj_test/sync/SunshineSyncUtils.java
+++ app/src/main/java/com/summit_khj_test/sync/SunshineSyncUtils.java
@@ -2,8 +2,49 @@ package com.summit_khj_test.sync;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import com.summit_khj_test.data.WeatherContract;
 
 public class SunshineSyncUtils {
+
+    //동기화 여부
+    private static boolean sInitialized;
+
+    //주기적인 동기화 작업을 생성하고 동기화 필요 여부 확인
+    synchronized public static void initialize(final Context context) {
+        if (sInitialized)
+            return;
+        sInitialized = true;
+
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Uri forecastQueryUri = WeatherContract.WeatherEntry.CONTENT_URI;
+
+                String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
+                String selectionStatement = WeatherContract.WeatherEntry
+                        .getSqlSelectForTodayOnwards();
+
+                Cursor cursor = context.getContentResolver().query(
+                        forecastQueryUri,
+                        projectionColumns,
+                        selectionStatement,
+                        null,
+                        null);
+
+                if (null == cursor || cursor.getCount() == 0) {
+                    startImmediateSync(context);
+                }
+
+                cursor.close();
+                return null;
+            }
+        }.execute();
+    }
 
     //비동기 실행을 위해 IntentService를 사용하여 즉시 동기화를 수행
     public static void startImmediateSync(final Context context) {
