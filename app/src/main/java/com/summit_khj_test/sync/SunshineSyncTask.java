6168bef3b31ca2e91e05b4d3eacfd7e690265f46
Kim Hak Jin <fjtl36@naver.com>
2020-11-01 11:49:25 +0900

    SunshineSyncTask.java 작업 완료

--- app/src/main/java/com/summit_khj_test/sync/SunshineSyncTask.java
+++ app/src/main/java/com/summit_khj_test/sync/SunshineSyncTask.java
@@ -1,8 +1,10 @@
 package com.summit_khj_test.sync;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 
+import com.summit_khj_test.data.WeatherContract;
 import com.summit_khj_test.utilities.NetworkUtils;
 import com.summit_khj_test.utilities.OpenWeatherJsonUtils;
 
@@ -20,6 +22,24 @@ public class SunshineSyncTask {
 
             ContentValues[] weatherValues = OpenWeatherJsonUtils
                     .getWeatherContentValuesFromJson(context, jsonWeatherResponse);
+
+            //만약 가져온 데이터에 오류가 없다면
+            if (weatherValues != null && weatherValues.length != 0) {
+                ContentResolver sunshineContentResolver = context.getContentResolver();
+
+                //과거 데이터 삭제
+                sunshineContentResolver.delete(
+                        WeatherContract.WeatherEntry.CONTENT_URI,
+                        null,
+                        null);
+
+                //새로운 데이터 삽입
+                sunshineContentResolver.bulkInsert(
+                        WeatherContract.WeatherEntry.CONTENT_URI,
+                        weatherValues);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
